ae7cc70ab8d9abd81f6e20c246f1cbcb5578e079
Tomas Votroubek <tomas.votroubek@outlook.com>
2025-08-14 01:47:28 +0200

    uncover chasnov20 b 1

--- .test/test_ll_chasnov20.jl
+++ .test/test_ll_chasnov20.jl
@@ -20,3 +20,27 @@ function ex_chasnov20_5_2()
 
     (u1, u2), (dom_nneg, dom_nneg), (dom_null, dom_null), (1, 1)
 end
+
+function ex_chasnov20_b_1()
+    # pure equilibrium at x=0.5, y=0.5
+
+    dom_nneg(x) = (x[1], 1-x[1])
+    dom_null(x) = 0
+
+    e = exp(1)
+    soft(x) = [e^(10*x)/(e^(10*x)+e^(10*(1-x))), e^(10*(1-x))/(e^(10*x)+e^(10*(1-x)))]
+
+    A = [1 -1; -1 1]
+
+    u1(x, y) = -soft(y[1])' * A * soft(x[1])
+    u2(x, y) = -u1(x,y)
+
+    (u1, u2), (dom_nneg, dom_nneg), (dom_null, dom_null), (1, 1)
+end
+#=
+utils, nneg, null, dims = ex_chasnov20_b_1()
+quack = Quack.quack_oracle(utils, nneg, null, dims, start=([(0.5,)],[(0.5,)]))
+@show cnt, (actions, mixed, vals, best) = Quack.until_eps(quack, 1e-3)
+
+prettyprints(actions, mixed)
+=#
